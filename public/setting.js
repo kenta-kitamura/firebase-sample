d844852ce6b5ca8ba090938304668b3d098da79e
kenta-kitamura <kitamura@startup-technology.com>
2017-08-16 12:08:47 +0900

    first commit

---
+++ public/setting.js
@@ -0,0 +1,8 @@
+var config = {
+  apiKey: "xxx",
+  authDomain: "xxx.firebaseapp.com",
+  databaseURL: "https://xxx.firebaseio.com",
+  projectId: "xxx-000000",
+  storageBucket: "xxx.appspot.com",
+  messagingSenderId: "0000000000"
+};
